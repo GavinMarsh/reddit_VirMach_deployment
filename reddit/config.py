e2bb703c179325e70475abfe385200aa4cc024cb
Gavin <gavin@local_mac>
2019-03-11 12:22:05 +0000

    added a copy of config to the sub reddit directory and also added a local_mac.wsgi file so that i can also run the app locally while developing

--- reddit/config.py
+++ reddit/config.py
@@ -1,10 +1,9 @@
 ## MongoEngine Note: include db name in URI; it overwrites all others
 
 
-###########################################################
-## THIS CONFIG COPY IS HERE SO THAT APCHE2 WSGI WILL RUN ##
-###########################################################
-
+##########################################################
+## THIS CONFIG COPY IS HERE SO THAT WSGI LOCAL WILL RUN ##
+##########################################################
 
 class Config(object):
     MONGODB_HOST = 'mongodb://localhost:27017/gm-sandbox'
