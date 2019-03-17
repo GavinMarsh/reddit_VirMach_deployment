6255c950ce5d00035348e69293ea77dd2c682fd4
Gavin <gavin@local_mac>
2019-03-17 15:06:35 +0000

    dev

--- config.py
+++ config.py
@@ -7,16 +7,7 @@
 
 
 class Config(object):
-    MONGODB_HOST = """
-        const MongoClient = require(‘mongodb’).MongoClient;
-const uri = "mongodb+srv://admin:<password>@cluster0-zhceb.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-"""
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     DEBUG = False
     TESTING = False
 
