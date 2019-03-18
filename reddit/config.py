08ae737c15594d7802daeed9d44a8ecfbfa7b48f
Gavin <gavin@local_mac>
2019-03-18 17:32:47 +0000

    dev

--- reddit/config.py
+++ reddit/config.py
@@ -1,31 +1,31 @@
 ## MongoEngine Note: include db name in URI; it overwrites all others
-
+import pymongo
 
 ##########################################################
 ## THIS CONFIG COPY IS HERE SO THAT WSGI LOCAL WILL RUN ##
 ##########################################################
 
 class Config(object):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox
+    client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    MONGODB_HOST = client.gm_sandbox
 
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox
+    client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    MONGODB_HOST = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
     MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox = client.gm_sandbox
+    client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    MONGODB_HOST = client.gm_sandbox = client.gm_sandbox
 
     DEBUG = True
 
