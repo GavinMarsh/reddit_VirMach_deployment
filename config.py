9656019d4a2515e2e4e96928eeb0ce37aacc7acc
GavinMarsh <39089696+GavinMarsh@users.noreply.github.com>
2019-03-18 17:44:54 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@
 
 
 class Config(object):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #db = client.gm_sandbox
 
@@ -17,15 +17,15 @@ class Config(object):
     TESTING = False
 
 class ProductionConfig(Config):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #db = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #db = client.gm_sandbox
 
