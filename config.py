8a91f2a70e5c3e1b61a208bedcf81658eb3181d1
GavinMarsh <39089696+GavinMarsh@users.noreply.github.com>
2019-03-20 15:41:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,26 +8,26 @@ from pymongo import MongoClient
 
 
 class Config(object):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    client = MongoClient(MONGODB_HOST)
-    db = client.gm_sandbox
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    #client = MongoClient(MONGODB_HOST)
+    #db = client.gm_sandbox
 
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    client = MongoClient(MONGODB_HOST)
-    db = client.gm_sandbox
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    #client = MongoClient(MONGODB_HOST)
+    #db = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
-    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    client = MongoClient(MONGODB_HOST)
-    db = client.gm_sandbox
+    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    #client = MongoClient(MONGODB_HOST)
+    #db = client.gm_sandbox
 
     DEBUG = True
 
