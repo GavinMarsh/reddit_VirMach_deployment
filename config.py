41c4cbe13fe8665ceeaf4c8b52b152db7704f5d2
GavinMarsh <39089696+GavinMarsh@users.noreply.github.com>
2019-03-20 16:19:15 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
 ## MongoEngine Note: include db name in URI; it overwrites all others
-from pymongo import MongoClient
+import pymongo
 
-
-###########################################################
-## THIS CONFIG COPY IS HERE SO THAT APCHE2 WSGI WILL RUN ##
-###########################################################
-
+##########################################################
+## THIS CONFIG COPY IS HERE SO THAT WSGI LOCAL WILL RUN ##
+##########################################################
 
 class Config(object):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = MongoClient(MONGODB_HOST)
-    #db = client.gm_sandbox
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
+    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    #MONGODB_HOST = client.gm_sandbox
 
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = MongoClient(MONGODB_HOST)
-    #db = client.gm_sandbox
+    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    #MONGODB_HOST = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = MongoClient(MONGODB_HOST)
-    #db = client.gm_sandbox
+    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
+    #MONGODB_HOST = client.gm_sandbox = client.gm_sandbox
 
     DEBUG = True
 
