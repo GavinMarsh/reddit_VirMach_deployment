b0fa7ed3451379c1172089ea45df98f94292f104
Gavin <gavin@local_mac>
2019-03-20 17:06:12 +0000

    dev

--- reddit/config.py
+++ reddit/config.py
@@ -7,27 +7,23 @@ import pymongo
 
 class Config(object):
     #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    # below line is working for reading from MongoDB Atlas collection.
     MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox
 
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
     #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    # below line is working for reading from MongoDB Atlas collection.
     MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
-    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
     #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    # below line is working for reading from MongoDB Atlas collection.
     MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
-    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
-    #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
-    #MONGODB_HOST = client.gm_sandbox = client.gm_sandbox
+
 
     DEBUG = True
 
