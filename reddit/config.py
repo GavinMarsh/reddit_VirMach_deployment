373809a467683e67f26697d11a8889b4c7c3737b
Gavin <gavin@local_mac>
2019-03-20 16:15:36 +0000

    dev

--- reddit/config.py
+++ reddit/config.py
@@ -6,8 +6,8 @@ import pymongo
 ##########################################################
 
 class Config(object):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
-    #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #MONGODB_HOST = client.gm_sandbox
 
@@ -15,14 +15,16 @@ class Config(object):
     TESTING = False
 
 class ProductionConfig(Config):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #MONGODB_HOST = client.gm_sandbox
 
 # why is apache2 using this config ???
 class DevelopmentConfig(Config):
-    MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    #MONGODB_HOST = 'mongodb://localhost:27017/gm_sandbox'
+    MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/admin'
     #MONGODB_HOST = 'mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true'
     #client = pymongo.MongoClient("mongodb+srv://admin:P=12345678@cluster0-zhceb.mongodb.net/test?retryWrites=true")
     #MONGODB_HOST = client.gm_sandbox = client.gm_sandbox
